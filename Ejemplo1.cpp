5b8797d470c39e0a095ff13b1b8659c93ec4f878
Hanssell89 <hanssell.llanes16143@est.uca.edu.ni>
2023-04-24 07:45:00 -0600

    Agrege la carpeta Ejemplos.cpp

---
+++ Ejemplo1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /*code*/
+    cout << "Hola mundo desde la UCA" << endl;
+    return 0;
+}
---
+++ Ejemplo2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* Mi presentacon */
+    system("color 13");
+    cout << "C U R R I C U L U M"<< endl;
+    cout << "Datos Generales" << endl;
+    cout << "===============" << endl;
+    cout << "Nombres: Hanssell Ariel" << endl;
+    cout << "Apellidos: Llanes Gutierrez" << endl;
+    return 0;
+}
